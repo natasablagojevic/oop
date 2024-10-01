709e1a235cab43e642bb6f50c9e9f98be9e4d807
natasablagojevic <blagojevicnatasa360@gmail.com>
2024-10-01 10:08:16 +0200

    vezbe 03

--- Vezbe03/src/klase/Main.java
+++ Vezbe03/src/klase/Main.java
@@ -1,4 +1,8 @@
 package klase;
 
 public class Main {
+    public static void main(String[] args) {
+        Student s = new Student();
+
+    }
 }
--- Vezbe03/src/klase/Student.java
+++ Vezbe03/src/klase/Student.java
@@ -1,4 +1,29 @@
 package klase;
 
 public class Student {
+    private String name;
+    private String lastName;
+    private String index;
+
+    // konstruktor
+    public Student() {
+
+    }
+
+    public Student(String name, String lastName, String index) {
+        this.name = name;
+        this.lastName = lastName;
+        this.index = index;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Name: " + name + "\n");
+        sb.append("Last Name: " + lastName + "\n");
+        sb.append("Index: " + index + "\n");
+
+        return sb.toString();
+    }
 }
