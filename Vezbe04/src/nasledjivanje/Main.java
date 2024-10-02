3c363f314ba7911336a9a5a74902bb96e177d079
natasablagojevic <blagojevicnatasa360@gmail.com>
2024-10-02 10:53:56 +0200

    vezbe 04

--- Vezbe04/src/nasledjivanje/Main.java
+++ Vezbe04/src/nasledjivanje/Main.java
@@ -1,4 +1,13 @@
 package nasledjivanje;
 
 public class Main {
+    public static void main(String[] args) {
+        Tacka t = new Tacka();
+        Tacka t1 = new Tacka(4.1, 1.1);
+
+        System.out.println(t);
+        System.out.println(t1);
+
+        System.out.println(Tacka.brojTacaka);
+    }
 }
--- Vezbe04/src/nasledjivanje/Tacka.java
+++ Vezbe04/src/nasledjivanje/Tacka.java
@@ -1,4 +1,29 @@
 package nasledjivanje;
 
 public class Tacka {
+    private double x;
+    private double y;
+
+    public static int brojTacaka;
+
+    public Tacka() {
+        this.x = 0.0;
+        this.y = 0.0;
+        brojTacaka++;
+    }
+
+    public Tacka(double x, double y) {
+        this.x = x;
+        this.y = y;
+        brojTacaka++;
+    }
+
+    public static int getBrojTacaka() {
+        return brojTacaka;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + this.x + ", " + this.y + ")";
+    }
 }
